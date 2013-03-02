4d99811ebd579e079c85b931efa797d35cc5abab
Simon Hafner <hafnersimon@gmail.com>
2013-03-02 00:56:54 -0600

    wildcard imports ftw!

--- src/main/scala/tshrdlu/Lucene.scala
+++ src/main/scala/tshrdlu/Lucene.scala
@@ -1,21 +1,11 @@
 package tshrdlu
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer
-import org.apache.lucene.document.Document
-import org.apache.lucene.document.Field
-import org.apache.lucene.document.StringField
-import org.apache.lucene.document.TextField
-import org.apache.lucene.index.DirectoryReader
-import org.apache.lucene.index.IndexReader
-import org.apache.lucene.index.IndexWriter
-import org.apache.lucene.index.IndexWriterConfig
+import org.apache.lucene.document._
+import org.apache.lucene.index._
 import org.apache.lucene.queryparser.classic.QueryParser
-import org.apache.lucene.search.IndexSearcher
-import org.apache.lucene.search.Query
-import org.apache.lucene.search.ScoreDoc
-import org.apache.lucene.search.TopScoreDocCollector
-import org.apache.lucene.store.Directory
-import org.apache.lucene.store.RAMDirectory
+import org.apache.lucene.search._
+import org.apache.lucene.store._
 import org.apache.lucene.util.Version
 import twitter4j.Status
 import scala.collection.JavaConversions._

7d5ffaceef91209e6c9ac96ce15e9e45c625409b
Simon Hafner <hafnersimon@gmail.com>
2013-03-05 17:59:11 -0600

    english analyzer

--- src/main/scala/tshrdlu/Lucene.scala
+++ src/main/scala/tshrdlu/Lucene.scala
@@ -1,21 +1,21 @@
 package tshrdlu
 
-import org.apache.lucene.analysis.standard.StandardAnalyzer
 import org.apache.lucene.document._
 import org.apache.lucene.index._
 import org.apache.lucene.queryparser.classic.QueryParser
 import org.apache.lucene.search._
 import org.apache.lucene.store._
+import org.apache.lucene.analysis.en._
 import org.apache.lucene.util.Version
 import twitter4j.Status
 import scala.collection.JavaConversions._
 
 object Lucene {
   val index = new RAMDirectory()
-  val analyzer = new StandardAnalyzer(Version.LUCENE_40)
-  val config = new IndexWriterConfig(Version.LUCENE_40, analyzer)
+  val analyzer = new EnglishAnalyzer(Version.LUCENE_41)
+  val config = new IndexWriterConfig(Version.LUCENE_41, analyzer)
   val writer = new IndexWriter(index, config)
-  val parser = new QueryParser(Version.LUCENE_40, "text", analyzer)
+  val parser = new QueryParser(Version.LUCENE_41, "text", analyzer)
 
   def write(tweets: Iterable[String]) {
     val documents = asJavaIterable(tweets.map({tweet =>

72c7a0a37e63fbed25abfa4025ba7ca9626be66a
myall86 <ckc8687@gmail.com>
2013-03-06 20:03:31 -0600

    new

--- src/main/scala/tshrdlu/twitter/Bot.scala
+++ src/main/scala/tshrdlu/twitter/Bot.scala
@@ -25,6 +25,7 @@ import scala.concurrent.duration._
 import akka.util.Timeout
 
 import tshrdlu.Lucene
+import tshrdlu.util.{English,SimpleTokenizer}
 
 /**
  * Base trait with properties default for Configuration.
@@ -37,6 +38,21 @@ trait TwitterInstance {
 case class ScrubGeo(userId: Long, upToStatusId: Long)
 case class TrackLimitationNotice(numberOfLimitedStatuses: Int)
 
+case class Block(source: User, blockedUser: User)
+case class DeletionNotice(directMessageId: Long, userId: Long)
+case class Favorite(source: User, target: User, favoritedStatus: Status)
+case class Follow(source: User, followedUser: User)
+case class Unblock(source: User, unblockedUser: User)
+case class Unfavorite(source: User, target: User, unfavoritedStatus: Status)
+case class UserListCreation(listOwner: User, list: UserList)
+case class UserListDeletion(listOwner: User, list: UserList)
+case class UserListMemberAddition(addedMember: User, listOwner: User, list: UserList)
+case class UserListMemberDeletion(deletedMember: User, listOwner: User, list: UserList)
+case class UserListSubscription(subscriber: User, listOwner: User, list: UserList)
+case class UserListUnsubscription(subscriber: User, listOwner: User, list: UserList)
+case class UserListUpdate(listOwner: User, list: UserList)
+
+
 // Streaming the statuses to the actors.
 class Streamer(actor: ActorRef) extends StreamInstance {
   stream.addListener(new StatusListener() {
@@ -47,6 +63,33 @@ class Streamer(actor: ActorRef) extends StreamInstance {
     def onTrackLimitationNotice(int: Int) = actor ! TrackLimitationNotice(int)
     def onException(ex: Exception) = actor ! akka.actor.Status.Failure(ex)
   })
+
+	stream.addListener(new UserStreamListener()
+	{
+		def onStatus(status: Status) = actor ! status
+    def onDeletionNotice(notice: StatusDeletionNotice) = actor ! notice
+    def onScrubGeo(userId: Long, upToStatusId: Long) = actor ! ScrubGeo(userId, upToStatusId)
+    def onStallWarning(warning: StallWarning) = actor ! warning
+    def onTrackLimitationNotice(int: Int) = actor ! TrackLimitationNotice(int)
+    def onException(ex: Exception) = actor ! akka.actor.Status.Failure(ex)
+
+		def onBlock(source: User, blockedUser: User) = actor ! Block(source, blockedUser)
+  def onDeletionNotice(directMessageId: Long, userId: Long) = actor ! DeletionNotice(directMessageId, userId)
+  def onDirectMessage(directMessage: DirectMessage) = actor ! directMessage
+  def onFavorite(source: User, target: User, favoritedStatus: Status) = actor ! Favorite(source, target, favoritedStatus)
+  def onFollow(source: User, followedUser: User) = actor ! Follow(source, followedUser)
+  def onFriendList(friendIds: Array[Long]) = actor ! friendIds
+  def onUnblock(source: User, unblockedUser: User) = actor ! Unblock(source: User, unblockedUser: User)
+  def onUnfavorite(source: User, target: User, unfavoritedStatus: Status) = actor ! Unfavorite(source, target, unfavoritedStatus)
+  def onUserListCreation(listOwner: User, list: UserList) = actor ! UserListCreation(listOwner, list)
+  def onUserListDeletion(listOwner: User, list: UserList) = actor ! UserListDeletion(listOwner, list)
+  def onUserListMemberAddition(addedMember: User, listOwner: User, list: UserList) = actor ! UserListMemberAddition(addedMember, listOwner, list)
+  def onUserListMemberDeletion(deletedMember: User, listOwner: User, list: UserList) = actor ! UserListMemberDeletion(deletedMember, listOwner, list)
+  def onUserListSubscription(subscriber: User, listOwner: User, list: UserList) = actor ! UserListSubscription(subscriber, listOwner, list)
+  def onUserListUnsubscription(subscriber: User, listOwner: User, list: UserList) = actor ! UserListUnsubscription(subscriber, listOwner, list)
+  def onUserListUpdate(listOwner: User, list: UserList) = actor ! UserListUpdate(listOwner, list)
+  def onUserProfileUpdate(updatedUser: User) = actor ! updatedUser
+	})
 }
 
 /**
@@ -111,12 +154,52 @@ class Receiver extends Actor {
 }
 
 class Replier extends Actor {
+  // Recognize a follow command
+  lazy val FollowRE = """(?i)(?<=follow)(\s+(me|@[A-Za-z\d_]+))+""".r
+
+  // Pull just the lead mention from a tweet.
+  lazy val StripLeadMentionRE = """(?:)^@[A-Za-z\d_]+\s(.*)$""".r
+
   def receive = {
     // If this becomes a bottleneck, create subworkers.
     case status: Status => {
       // TODO: Reply n stuff.
+	val username = "ckcuong_anlp" // TODO: need to get the real username
+	println("New status: " + status.getText)
+    	val replyName = status.getInReplyToScreenName
+    	if (replyName == username) {
+      		println("*************")
+      		println("New reply: " + status.getText)
+      		val text = "@" + status.getUser.getScreenName + " " + doActionGetReply(status)
+      		println("Replying: " + text)
+      		val reply = new StatusUpdate(text).inReplyToStatusId(status.getId)
+//      		twitter.updateStatus(reply)
+    	}
     }
   }
+
+   /**
+   * A method that possibly takes an action based on a status
+   * it has received, and then produces a response.
+   */
+  def doActionGetReply(status: Status) = {
+    val text = status.getText.toLowerCase
+    val followMatches = FollowRE.findAllIn(text)
+     try {
+	val StripLeadMentionRE(withoutMention) = text
+	val query = SimpleTokenizer(withoutMention)
+	    .filter(_.length > 2)
+//	    .toSet
+//	    .take(3)
+	    .toList
+	    .mkString(" ")
+	Lucene.read(query)
+      }	catch { 
+	case _: Throwable => "???"
+      }
+  
+  }
+
 }
 
 class Collector extends Actor {
@@ -133,11 +216,12 @@ class Collector extends Actor {
 }
 
 class LuceneWriter extends Actor {
+	// Pull the RT and mentions from the front of a tweet.
+	lazy val StripMentionsRE = """(?:)(?:RT\s)?(?:(?:@[A-Za-z\d_]+\s))+(.*)$""".r
+
   def receive = {
     case batch: List[Status] => {
       // TODO: Filter & write to lucene
-	// Pull the RT and mentions from the front of a tweet.
-  	lazy val StripMentionsRE = """(?:)(?:RT\s)?(?:(?:@[A-Za-z\d_]+\s))+(.*)$""".r
 	 val useableTweets = batch
       .map(_.getText)
       .map {
